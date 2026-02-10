0b3d71509ec9d7b925add4bdb3272debfa083f71
pg2103 <prakhar.gupta.212003@gmail.com>
2026-02-10 12:21:11 +0530

    add assignment minimal code for react

---
+++ tests/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+test('renders heading', () => {
+  render(<App />)
+  expect(screen.getByText(/react ci pipeline works/i)).toBeInTheDocument()
+})
